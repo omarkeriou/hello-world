52486151ddc44be582363368a50fa47706b0ddc3
omarkeriou <omar.keriou@outlook.de>
2020-10-31 18:37:57 +0100

    a new cpp-file is added and the Hello_world.cpp has been modified

---
+++ Goodbye.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+using namespace std;
+int main (void){
+cout<<'Goodbye everyone from the new branch "branch_1"'<<endl;
+}
--- Hello_world.cpp
+++ Hello_world.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
 int main(void){
-cout <<'Hello world from my first cloned Project'<< endl;
+cout <<'Hello world from my first cloned Project with modification'<< endl;
 return 0;
 }
